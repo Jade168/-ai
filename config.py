edaae2ecf127b96b3de05f5b41d9b00752244b32
Jade168 <128027202+Jade168@users.noreply.github.com>
2026-02-23 16:36:25 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# 智譜API Key
+ZHIPU_API_KEY = ""
+
+# 每日定時分析自選股列表
+WATCH_LIST = ["TSLA", "06809", "NVDA", "AAPL", "09888"]
+
+# 數律模型核心參數
+CORE_NUMBER = 1836.15
+RATIO = 0.183615
+
+# 服務端口
+SERVER_PORT = 1836
